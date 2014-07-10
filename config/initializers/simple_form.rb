eb4dd8fa789816f154a9b77eff54d2eff881ba2f
Marlon Estrada <marlon.estrada@gmail.com>
2014-07-10 08:54:14 +0000

    Added validation to courses. Need to add glyphicons to bootstrap for some effects.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -6,7 +6,7 @@ SimpleForm.setup do |config|
   # stack. The options given below are used to wrap the
   # whole input.
   config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: 'has-error' do |b|
+    hint_class: :field_with_hint, error_class: 'has-error has-feedback' do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
@@ -41,8 +41,8 @@ SimpleForm.setup do |config|
 
     ## Inputs
     b.use :label_input
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
+    b.use :hint,  wrap_with: { tag: :span, class: 'bg-info'}
+    b.use :error, wrap_with: { tag: :div, class: 'alert alert-danger' }
   end
 
   # The default wrapper to be used by the FormBuilder.
@@ -141,5 +141,5 @@ SimpleForm.setup do |config|
   # config.cache_discovery = !Rails.env.development?
 
   # Default class for inputs
-  config.input_class = 'form-control'
+  config.input_class = 'form-control input-lg'
 end
