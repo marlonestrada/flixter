916912eb28dc4666ca2daf4ccd6f093b94eb296e
Marlon Estrada <marlon.estrada@gmail.com>
2014-07-22 08:21:49 +0000

    Students can enroll in classes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Flixter::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
-  resources :courses, :only => [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: [:create]
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
-    resources :courses, :only => [:new, :create, :show] do
+    resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
   end
