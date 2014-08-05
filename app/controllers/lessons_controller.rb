7a47c6aef1d712fe5c30d5b5d6e006bfab27a555
Marlon Estrada <marlon.estrada@gmail.com>
2014-08-05 03:53:23 +0000

    Students must now be enrolled in courses to watch the lesson videos.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course
+
   def show
   end
 
   private
+
+  def require_enrolled_in_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in the course to watch the lessons.'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
