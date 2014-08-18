1b4a953f8f758563db2f3dc0fed776e18c9ddc27
Marlon Estrada <marlon.estrada@gmail.com>
2014-08-18 14:13:55 +0000

    Sections are sortable

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -21,6 +21,7 @@ class Instructor::LessonsController < ApplicationController
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
@@ -33,7 +34,6 @@ class Instructor::LessonsController < ApplicationController
       render text: 'Unauthorized', status: :unauthorized
     end
   end
-
 
   helper_method :current_section
   def current_section
